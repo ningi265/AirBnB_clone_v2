97907e9d64bce7f934d6f3679ddd2c517f94d646
ningi265 <ningintonga@gmail.com>
2024-08-22 05:03:44 +0000

    route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,7 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def hello_hbnb():
     """ a function that prints hello `hbnb at the root"""
     return "Hello HBNB!"
