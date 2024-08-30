595f0815e8d468b2152883c5c73949a70ee90806
legendmw <brianmtonga592@gmail.com>
2024-08-30 11:09:48 +0200

    all

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,17 @@
-s module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+#!/usr/bin/python3
+"""
+initialize the models package
+"""
+
+from os import getenv
 
 
-storage = FileStorage()
+storage_t = getenv("HBNB_TYPE_STORAGE")
+
+if storage_t == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 storage.reload()
--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""a script that starts a flask web application"""
-from flask import Flask
+"""
+start Flask application
+"""
 
+from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/airbnb-onepage/', strict_slashes=False)
-def hello_hbnb():
-    """ a function that prints hello `hbnb at the root"""
-    return "Hello HBNB!"
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
 
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
